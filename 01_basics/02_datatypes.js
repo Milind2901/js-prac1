8503baabb89edc982b78730b0a9c8b355b3c3a4f
Milind <71056116+Milind2901@users.noreply.github.com>
2024-02-08 14:06:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" ;  // treat all code as the newer version 
+
+// alert(3+3) // we are using nodejs not browser 
+
+let name = "milind"
+let age = 22
+let isloggedIn = true 
+
+// primitive datatypes
+// number => 2 to the power 53 
+// bigInt => storing bigger integer values 
+// string => ""
+// boolean => true or false 
+// null => standalone value -- signifying empty or nothing 
+// undefined => value hasn't been assigned yet 
+// symbol => unique 
+
+
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined 
