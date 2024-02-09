21ab3703024e6023484a4a3f5b68f71ef24a651e
Milind <71056116+Milind2901@users.noreply.github.com>
2024-02-09 18:46:32 +0000

    changed comment format

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -22,24 +22,26 @@ let someStringnumber = String(someNumber)
 console.log(someStringnumber);
 console.log(typeof someStringnumber);
 
+/*
+  "02" > 1 true 
+  "2" > 1  ~true 
 
-// "02" > 1 true 
-// "2" > 1  ~true 
-
-// for comparison of two values their datatype "must be same" 
-
-
-// null > 0    false     (0 > 0 is false)                 // comparison operators convert null to a number i.e. to 0 or to Nan 
-// null == 0   false                                         equal operator treats null as null itself 
-// null >= 0    true    ( 0 >=0  is true)
-
-// undefined == 0 , > 0,  < 0    false 
-
-// comparators ( > , < , >= , <= ) and equality check ( == or ===)  => both work differently in js 
-
-// strict check ===   checks both datatype and value   
-// "2" == 2    true  ( doesn't check datatype and converts string to number to check )
-// "2" === 2   false 
+   -> for comparison of two values their datatype "must be same" 
 
 
-// in summary - beware of such comparisons and avoid them 
+ null > 0    false     (0 > 0 is false)                 // comparison operators convert null to a number i.e. to 0 or to Nan 
+ null == 0   false                                         equal operator treats null as null itself 
+ null >= 0    true    ( 0 >=0  is true)
+
+ undefined == 0 , > 0,  < 0    false 
+
+ -> comparators ( > , < , >= , <= ) and equality check ( == or ===)  => both work differently in js 
+
+ strict check ===   checks both datatype and value   
+ "2" == 2    true  ( doesn't check datatype and converts string to number to check )
+ "2" === 2   false 
+
+
+ -> in summary - beware of such comparisons and avoid them 
+
+ */ 
