f3fae8bc2454a85ec247c2bb14ecb3592b15cb14
Milind <71056116+Milind2901@users.noreply.github.com>
2024-02-09 06:02:42 +0000

    Conversion of Datatype

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,23 @@
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let loggedIn = 1
+
+let isloggedIn = Boolean(loggedIn)
+
+console.log(isloggedIn)
+
+// 1 => true 
+// 0 => false 
+// "" => false 
+// "Milind" => true 
+
+let someNumber = 33 
+let someStringnumber = String(someNumber)
+
+console.log(someStringnumber);
+console.log(typeof someStringnumber);
+
+
