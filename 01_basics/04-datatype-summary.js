46a54f0d11c0ff0ded4a7497c6fc56fc54e8ee80
Milind <71056116+Milind2901@users.noreply.github.com>
2024-02-10 11:25:11 +0000

    Datatype summary

---
+++ 01_basics/04-datatype-summary.js
@@ -0,0 +1,40 @@
+/* 
+
+On basis of how data is stored in memory and how it can be accesed ,
+data is divide into 2 types - 
+
+-- primitive                 ( original value ke sang chedchad "nahi" kar sakte memory mei )
+-- non primitive              (original value ke sang chedchad kar sakte hai memory mei )
+
+# Primitive : 7 types  - Strings  , Numbers , Boolean , Null , Undefined , Symbol (used to set uniqueness), BigInt , 
+
+# Reference Type/ Non Primitive  ( direct access of valuess in memory is allocated )
+- Arrays , Objects , Functions 
+
+=> JS is dynamically typed language cause the datatype of variable is determined by the value it holds during the runtime 
+   and can change throughout the protgram as we assign different values to it 
+
+
+Return type of variables in JavaScript
+
+1) Primitive Datatypes
+
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object (*****)
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function(object)
+       Object  =>  object
+
+*/ 
+
+
+
+    
+
