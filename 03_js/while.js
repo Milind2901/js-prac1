91aa7debcc9baabdf0308cc51521be443660a012
Milind <71056116+Milind2901@users.noreply.github.com>
2024-03-12 11:32:39 +0000

    do_while

---
+++ 03_js/while.js
@@ -0,0 +1,22 @@
+// While & do-while
+
+while (condition) {
+    
+}
+
+
+// Do while 
+// Pehle do , yani ki pehle kaam hoga fir condition check hogi 
+do {
+    
+} while (condition);
+
+// eg 
+
+let score = 11 
+
+do {
+    console.log(`Sore is ${score}`);
+    score++
+} while (score <=10);                       // Result : 11
+
