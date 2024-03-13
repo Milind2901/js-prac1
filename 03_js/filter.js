a0bd0a5b292659e0f2089eb4784d23787f4ff463
Milind <71056116+Milind2901@users.noreply.github.com>
2024-03-13 14:20:09 +0000

    filter fn

---
+++ 03_js/filter.js
@@ -0,0 +1,17 @@
+// FILTER FN 
+
+const myNumber = [1,2,3,4,5]
+
+const myNums = myNumber.filter( (num) => num > 3)   // syntax is following implicit return
+
+console.log(myNums);  // Result : [3,4,5]
+
+// another syntax with explicit return 
+
+const newNums = myNumber.filter( (num) => {
+    return num > 3 
+})
+
+console.log(newNums)  // Result : [3,4,5]
+
+
